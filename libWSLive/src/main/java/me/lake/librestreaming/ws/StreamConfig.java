470a44a109cc0f706882acf633d3978bce442c38
sxh_droid <sxh_droid@163.com>
2020-08-13 17:28:02 +0800

    修改默认参数配置

--- libWSLive/src/main/java/me/lake/librestreaming/ws/StreamConfig.java
+++ libWSLive/src/main/java/me/lake/librestreaming/ws/StreamConfig.java
@@ -1,7 +1,6 @@
 package me.lake.librestreaming.ws;
 
 import android.content.Context;
-import android.content.res.Configuration;
 import android.hardware.Camera;
 
 import me.lake.librestreaming.model.RESConfig;
@@ -24,19 +23,19 @@ public class StreamConfig {
 
         public static final int DEFAULT_RENDER_MODE = RESConfig.RenderingMode.OpenGLES;
 
-        public static final int DEFAULT_PREVIEW_WIDTH = 1280;
+        public static final int DEFAULT_PREVIEW_WIDTH = 1920;
 
-        public static final int DEFAULT_PREVIEW_HEIGHT = 720;
+        public static final int DEFAULT_PREVIEW_HEIGHT = 1080;
 
-        public static final int DEFAULT_VIDEO_WIDTH = 640;
+        public static final int DEFAULT_VIDEO_WIDTH = 1920;
 
-        public static final int DEFAULT_VIDEO_HEIGHT = 360;
+        public static final int DEFAULT_VIDEO_HEIGHT = 1080;
 
-        public static final int DEFAULT_VIDEO_BITRATE =600 * 1024;
+        public static final int DEFAULT_VIDEO_BITRATE = 600 * 1024;
 
-        public static final int DEFAULT_VIDEO_FPS = 20;
+        public static final int DEFAULT_VIDEO_FPS = 24;
 
-        public static final int DEFAULT_VIDEO_GOP = 2;
+        public static final int DEFAULT_VIDEO_GOP = 1;
 
     }
 
@@ -51,20 +50,7 @@ public class StreamConfig {
         res.setVideoGOP(option.videoGOP);
         res.setDefaultCamera(option.cameraIndex);
         res.setRtmpAddr(option.streamUrl);
-
-        int frontDirection, backDirection;
-        Camera.CameraInfo cameraInfo = new Camera.CameraInfo();
-        Camera.getCameraInfo(Camera.CameraInfo.CAMERA_FACING_FRONT, cameraInfo);
-        frontDirection = cameraInfo.orientation;
-        Camera.getCameraInfo(Camera.CameraInfo.CAMERA_FACING_BACK, cameraInfo);
-        backDirection = cameraInfo.orientation;
-        if (context.getResources().getConfiguration().orientation == Configuration.ORIENTATION_PORTRAIT) {
-            res.setFrontCameraDirectionMode((frontDirection == 90 ? RESConfig.DirectionMode.FLAG_DIRECTION_ROATATION_270 : RESConfig.DirectionMode.FLAG_DIRECTION_ROATATION_90) | RESConfig.DirectionMode.FLAG_DIRECTION_FLIP_HORIZONTAL);
-            res.setBackCameraDirectionMode((backDirection == 90 ? RESConfig.DirectionMode.FLAG_DIRECTION_ROATATION_90 : RESConfig.DirectionMode.FLAG_DIRECTION_ROATATION_270));
-        } else {
-            res.setBackCameraDirectionMode((backDirection == 90 ? RESConfig.DirectionMode.FLAG_DIRECTION_ROATATION_0 : RESConfig.DirectionMode.FLAG_DIRECTION_ROATATION_180));
-            res.setFrontCameraDirectionMode((frontDirection == 90 ? RESConfig.DirectionMode.FLAG_DIRECTION_ROATATION_180 : RESConfig.DirectionMode.FLAG_DIRECTION_ROATATION_0) | RESConfig.DirectionMode.FLAG_DIRECTION_FLIP_HORIZONTAL);
-        }
+        res.setBackCameraDirectionMode(RESConfig.DirectionMode.FLAG_DIRECTION_ROATATION_0);
         return res;
     }
 
